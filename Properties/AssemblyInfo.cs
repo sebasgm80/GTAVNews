440ad35edb39004ab67959208f05606041638b09
Sebas <sebasgarcia80@gmail.com>
2026-09-05 17:25:18 +0200

    fix: convert to proper UWP project format

---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,11 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("GTAVNews")]
+[assembly: AssemblyDescription("GTA V News App for Xbox")]
+[assembly: AssemblyCompany("sebas")]
+[assembly: AssemblyProduct("GTAVNews")]
+[assembly: AssemblyCopyright("Copyright © sebas 2026")]
+[assembly: ComVisible(false)]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
